e5f7bee30ea3792dc21ebb9f8e1a230faf1b7a55
Somrithreach San <somrithreachsan@gmail.com>
2025-01-24 18:11:04 +0700

    file browser

--- app/dashboard/_components/file-browser.tsx
+++ app/dashboard/_components/file-browser.tsx
@@ -2,6 +2,7 @@
 import { useOrganization, useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 
+import { UploadButton } from "./upload-button";
 import { FileCard } from "./file-card";
 import Image from "next/image";
 import { GridIcon, Loader2, RowsIcon } from "lucide-react";
@@ -19,39 +20,17 @@ import {
 } from "@/components/ui/select";
 
 import { Label } from "@/components/ui/label";
-
 import { Doc } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
-import { usePathname } from "next/navigation";
-import { UploadButton } from "./upload-button";
-
-// Placeholder Component for Empty States
-function Placeholder({
-  context,
-}: {
-  context: "files" | "favorites" | "trash";
-}) {
-  const message = {
-    files: "You have no files, upload one now",
-    favorites: "You have no favorite files yet, add some!",
-    trash: "Your trash is empty, no files have been deleted.",
-  };
-
-  const imageSrc = {
-    files: "/Empty_State_Mascot_File.svg", // Change to appropriate image for files
-    favorites: "/Empty_State_Mascot_Favorite.svg", // Change to appropriate image for favorites
-    trash: "/Empty_State_Mascot_Trash.svg", // Change to appropriate image for trash
-  };
-
 
 function Placeholder() {
   return (
     <div className="flex flex-col gap-8 w-full items-center mt-24">
       <Image
         alt="an image of a picture and directory icon"
-        width="150"
-        height="150"
-        src="/Empty_State_Mascot.svg"
+        width="300"
+        height="300"
+        src="/empty.svg"
       />
       <div className="text-2xl">You have no files, upload one now</div>
       <UploadButton />
